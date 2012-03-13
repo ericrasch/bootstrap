c1e73f6f0b0f39d657259e3560de9a326434b766
Eric Rasch <ericrasch+github@gmail.com>
2012-03-13 16:36:11 -0500

    Adding social media links + icons.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
             <hr>
 
             <footer>
-                <p>&copy; Company 2012</p>
+                <p>&copy; Company 2012 | <a href="http://twitter.com/EricRasch" title="Follow me on Twitter @EricRasch"><img src="assets/ico/icons/social-logo-twitter.png" alt="Follow me on Twitter @EricRasch" width="16" height="16" /></a></p>
             </footer>
 
         </div> <!-- /container -->
