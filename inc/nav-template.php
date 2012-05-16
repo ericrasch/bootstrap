42525dba4b8fd929ec9e3cd1307865dea834bd17
Eric Rasch <ericrasch+github@gmail.com>
2012-05-16 14:00:45 -0500

    Changed the utility menu not to show if there's not a Custom Menu defined for it.

--- inc/nav-template.php
+++ inc/nav-template.php
@@ -1,7 +1,10 @@
-<?php wp_nav_menu( array(
-    'theme_location' => 'header_utility',
-    'container'       => false,
-    'menu_class'      => 'nav pull-right',
-    'menu_id'         => ''
-    )
-); ?>
+<?php
+	if ( has_nav_menu( 'header_utility' ) ) :
+		wp_nav_menu( array(
+		    'theme_location' => 'header_utility',
+		    'container'       => false,
+		    'menu_class'      => 'nav pull-right',
+		    'menu_id'         => ''
+		) );
+	endif;
+?>
